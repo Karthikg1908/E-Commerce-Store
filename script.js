6c95f75d0298d186a815d675677891eeb26c653b
Karthik <86306862+Karthikg1908@users.noreply.github.com>
2023-11-06 18:58:01 +0530

    commit

---
+++ script.js
@@ -0,0 +1,573 @@
+// Start adding Product for Kids
+const products = [
+    {
+        id: 1,
+        name: "Kurta for kids",
+        price: 299,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Kids/k1.jpg"
+    },
+    {
+        id: 2,
+        name: "Frock",
+        price: 459,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Kids/k2.jpg"
+    },
+	{
+        id: 1,
+        name: "Pant & Shirt",
+        price: 358,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Kids/k3.jpg"
+    },
+    {
+        id: 2,
+        name: "Mini Frock",
+        price: 199,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Kids/k4.jpg"
+    },
+	{
+        id: 1,
+        name: "suit for boys",
+        price: 789,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Kids/k5.jpg"
+    },
+    {
+        id: 2,
+        name: "Combo Frocks",
+        price: 899,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Kids/k6.jpg"
+    },
+	{
+        id: 1,
+        name: "T-shirt",
+        price: 125,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Kids/k7.jpg"
+    },
+    {
+        id: 2,
+        name: "Kurta",
+        price: 499,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Kids/k8.jpg"
+    },
+	{
+        id: 1,
+        name: "Shirt & pant",
+        price: 799,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Kids/k9.jpg"
+    },
+    {
+        id: 2,
+        name: "Kid dress",
+        price: 489,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Kids/k10.jpg"
+    },
+    // Add more products here
+];
+// End of adding Product for Kids
+
+// Start adding Product for Men's
+
+const products1 = [
+    {
+        id: 1,
+        name: "Blazor",
+        price: 1099,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Mens/m1.jpg"
+    },
+    {
+        id: 2,
+        name: "Combo T-shirts",
+        price: 740,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Mens/m2.jpg"
+    },
+	{
+        id: 1,
+        name: "Pink Shirt",
+        price: 299,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Mens/m3.jpg"
+    },
+    {
+        id: 2,
+        name: "Combo Shirts",
+        price: 1199,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Mens/m4.jpg"
+    },
+	{
+        id: 1,
+        name: "kurtha",
+        price: 455,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Mens/m5.jpg"
+    },
+    {
+        id: 2,
+        name: "Black Tshirt",
+        price: 199,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Mens/m6.jpg"
+    },
+	{
+        id: 1,
+        name: "Nike",
+        price: 999,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Mens/m7.jpg"
+    },
+    {
+        id: 2,
+        name: "Combo Pants",
+        price: 658,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Mens/m8.jpg"
+    },
+	{
+        id: 1,
+        name: "Jeans pant",
+        price: 499,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Mens/m9.jpg"
+    },
+    {
+        id: 2,
+        name: "Formal Pants",
+        price: 899,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Mens/m10.jpg"
+    },
+	,
+    // Add more products here
+];
+// End adding Product for Men's
+
+// Start adding Product for Women's
+const products2 = [
+    {
+        id: 1,
+        name: "Long gown",
+        price: 599,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Women/w1.jpg"
+    },
+    {
+        id: 2,
+        name: "Long Top",
+        price: 799,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Women/w2.jpg"
+    },
+	{
+        id: 1,
+        name: "Chudidhar",
+        price: 1200,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Women/w3.jpg"
+    },
+    {
+        id: 2,
+        name: "Short jeans top",
+        price: 349,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Women/w4.jpg"
+    },
+	{
+        id: 1,
+        name: "Full sleeves tops",
+        price: 399,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Women/w5.jpg"
+    },
+    {
+        id: 2,
+        name: "Jeans full length top",
+        price: 299,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Women/w6.jpg"
+    },
+	{
+        id: 1,
+        name: "Hoodie with pants",
+        price: 899,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Women/w7.jpg"
+    },
+    {
+        id: 2,
+        name: "Top with pant",
+        price: 999,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Women/w8.jpg"
+    },
+	{
+        id: 1,
+        name: "Top with pant",
+        price: 799,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Women/w9.jpg"
+    },
+    {
+        id: 2,
+        name: "Night Dress",
+        price: 459,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Women/w10.jpg"
+    },
+    // Add more products here
+];
+// End adding Product for Women's
+
+// Start adding Product for Eletronics
+
+const products3 = [
+    {
+        id: 1,
+        name: "Bluetooth earbuds",
+        price: 1099,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/electronics/e1.jpg"
+    },
+    {
+        id: 2,
+        name: "Kitchen gadgets",
+        price: 1999,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/electronics/e2.jpg"
+    },
+	{
+        id: 1,
+        name: "Speakers",
+        price: 2499,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/electronics/e3.jpg"
+    },
+    {
+        id: 2,
+        name: "Eletronics products",
+        price: 15000,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/electronics/e4.jpg"
+    },
+	{
+        id: 1,
+        name: "Laptop",
+        price: 55499,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/electronics/e5.jpg"
+    },
+    {
+        id: 2,
+        name: "Hp Laptop",
+        price: 69999,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/electronics/e6.jpg"
+    },
+	{
+        id: 1,
+        name: "LG Fridge",
+        price: 15000,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/electronics/e7.jpg"
+    },
+    {
+        id: 2,
+        name: "Samsung Fridge",
+        price: 21999,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/electronics/e8.jpg"
+    },
+	{
+        id: 1,
+        name: "Washing Machine",
+        price: 30699,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/electronics/e9.jpg"
+    },
+    {
+        id: 2,
+        name: "LED Tv",
+        price: 22000,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/electronics/e10.jpg"
+    },
+    // Add more products here
+];
+// End of adding Product for Eletronics
+
+// Start adding Product for Shoes
+
+const products4 = [
+    {
+        id: 1,
+        name: "Nike",
+        price: 2999,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/shoes/s1.jpg"
+    },
+    {
+        id: 2,
+        name: "Puma",
+        price: 2999,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/shoes/s2.jpg"
+    },
+	{
+        id: 1,
+        name: "Campus",
+        price: 699,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/shoes/s3.jpg"
+    },
+    {
+        id: 2,
+        name: "Paragon",
+        price: 399,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/shoes/s4.jpg"
+    },
+	{
+        id: 1,
+        name: "Casual",
+        price: 989,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/shoes/s5.jpg"
+    },
+    {
+        id: 2,
+        name: "Nike",
+        price: 3399,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/shoes/s6.jpg"
+    },
+	{
+        id: 1,
+        name: "Woodland",
+        price: 3999,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/shoes/s7.jpg"
+    },
+    {
+        id: 2,
+        name: "Metro",
+        price: 1599,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/shoes/s8.jpg"
+    },
+	{
+        id: 1,
+        name: "Liberty",
+        price: 3569,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/shoes/s9.jpg"
+    },
+    {
+        id: 2,
+        name: "Adidas",
+        price: 6999,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/shoes/s10.jpg"
+    },
+    // Add more products here
+];
+// End of adding Product for Shoes
+
+// Start adding Product for Toy's
+const products5 = [
+    {
+        id: 1,
+        name: "Panda",
+        price: 399,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Toys/i1.jpg"
+    },
+    {
+        id: 2,
+        name: "Dinosaur",
+        price: 749,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Toys/i2.jpg"
+    },
+	{
+        id: 1,
+        name: "Pikachu",
+        price: 699,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Toys/i3.jpg"
+    },
+    {
+        id: 2,
+        name: "Robot DOg",
+        price: 1999,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Toys/i4.jpg"
+    },
+	{
+        id: 1,
+        name: "Puppy",
+        price: 499,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Toys/i5.jpg"
+    },
+    {
+        id: 2,
+        name: "Doll",
+        price: 199,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Toys/i6.jpg"
+    },
+	{
+        id: 1,
+        name: "barbie",
+        price: 299,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Toys/i7.jpg"
+    },
+    {
+        id: 2,
+        name: "Teddy",
+        price: 499,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Toys/i8.jpg"
+    },
+	{
+        id: 1,
+        name: "Green Doll",
+        price: 259,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Toys/i9.jpg"
+    },
+    {
+        id: 2,
+        name: "Fidget Spinners",
+        price: 699,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Toys/i10.jpg"
+    },
+    // Add more products here
+];
+// End of adding Product for Toy's
+
+// Start adding Product for Travel
+const products6 = [
+    {
+        id: 1,
+        name: "Pouch",
+        price: 199,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Travel/t1.jpg"
+    },
+    {
+        id: 2,
+        name: "Bages",
+        price: 1999,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Travel/t2.jpg"
+    },
+	{
+        id: 1,
+        name: "Hand bag",
+        price: 1399,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Travel/t3.jpg"
+    },
+    {
+        id: 2,
+        name: "Mini Pouch",
+        price: 999,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Travel/t4.jpg"
+    },
+	{
+        id: 1,
+        name: "Luggage bag",
+        price: 2099,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Travel/t5.jpg"
+    },
+    {
+        id: 2,
+        name: "Travel Bag",
+        price: 3699,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Travel/t6.jpg"
+    },
+	{
+        id: 1,
+        name: "Mini Bag",
+        price: 699,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Travel/t7.jpg"
+    },
+    {
+        id: 2,
+        name: "Combo mini pouch's",
+        price: 999,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Travel/t8.jpg"
+    },
+	{
+        id: 1,
+        name: "Bag",
+        price: 199,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Travel/t9.jpg"
+    },
+    {
+        id: 2,
+        name: "Designed Pouch",
+        price: 499,
+        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Travel/t10.jpg"
+    },
+    // Add more products here
+];
+// End of adding Product for Travel
+
+// Load products and display them on the website
+const productList = document.getElementById("product-list");
+
+products.forEach(product => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+
+    productDiv.innerHTML = `
+        <img src="${product.image}" alt="${product.name}">
+        <h2>${product.name}</h2>
+        <p>Price: ₹${product.price.toFixed(2)}</p>
+        <button>Add to Cart</button>
+    `;
+
+    productList.appendChild(productDiv);
+});
+const productList1 = document.getElementById("product-list1");
+
+products1.forEach(product => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+
+    productDiv.innerHTML = `
+        <img src="${product.image}" alt="${product.name}">
+        <h2>${product.name}</h2>
+        <p>Price: ₹${product.price.toFixed(2)}</p>
+        <button>Add to Cart</button>
+    `;
+
+    productList1.appendChild(productDiv);
+});
+const productList2 = document.getElementById("product-list2");
+
+products2.forEach(product => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+
+    productDiv.innerHTML = `
+        <img src="${product.image}" alt="${product.name}">
+        <h2>${product.name}</h2>
+        <p>Price: ₹${product.price.toFixed(2)}</p>
+        <button>Add to Cart</button>
+    `;
+
+    productList2.appendChild(productDiv);
+});
+const productList3= document.getElementById("product-list3");
+
+products3.forEach(product => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+
+    productDiv.innerHTML = `
+        <img src="${product.image}" alt="${product.name}">
+        <h2>${product.name}</h2>
+        <p>Price: ₹${product.price.toFixed(2)}</p>
+        <button>Add to Cart</button>
+    `;
+
+    productList3.appendChild(productDiv);
+});
+const productList4= document.getElementById("product-list4");
+
+products4.forEach(product => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+
+    productDiv.innerHTML = `
+        <img src="${product.image}" alt="${product.name}">
+        <h2>${product.name}</h2>
+        <p>Price: ₹${product.price.toFixed(2)}</p>
+        <button>Add to Cart</button>
+    `;
+
+    productList4.appendChild(productDiv);
+});
+const productList5= document.getElementById("product-list5");
+
+products5.forEach(product => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+
+    productDiv.innerHTML = `
+        <img src="${product.image}" alt="${product.name}">
+        <h2>${product.name}</h2>
+        <p>Price: ₹${product.price.toFixed(2)}</p>
+        <button>Add to Cart</button>
+    `;
+
+    productList5.appendChild(productDiv);
+});
+
+const productList6= document.getElementById("product-list6");
+
+products6.forEach(product => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+
+    productDiv.innerHTML = `
+        <img src="${product.image}" alt="${product.name}">
+        <h2>${product.name}</h2>
+        <p>Price: ₹${product.price.toFixed(2)}</p>
+        <button>Add to Cart</button>
+    `;
+
+    productList6.appendChild(productDiv);
+});

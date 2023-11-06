68101223d213a6e4aa0c12b55834ca1554abb5f8
Karthik <86306862+Karthikg1908@users.noreply.github.com>
2023-11-06 19:09:04 +0530

    commit

--- script.js
+++ script.js
@@ -4,61 +4,61 @@ const products = [
         id: 1,
         name: "Kurta for kids",
         price: 299,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Kids/k1.jpg"
+        image: " k1.jpg"
     },
     {
         id: 2,
         name: "Frock",
         price: 459,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Kids/k2.jpg"
+        image: " k2.jpg"
     },
 	{
         id: 1,
         name: "Pant & Shirt",
         price: 358,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Kids/k3.jpg"
+        image: " k3.jpg"
     },
     {
         id: 2,
         name: "Mini Frock",
         price: 199,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Kids/k4.jpg"
+        image: " k4.jpg"
     },
 	{
         id: 1,
         name: "suit for boys",
         price: 789,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Kids/k5.jpg"
+        image: " k5.jpg"
     },
     {
         id: 2,
         name: "Combo Frocks",
         price: 899,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Kids/k6.jpg"
+        image: " k6.jpg"
     },
 	{
         id: 1,
         name: "T-shirt",
         price: 125,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Kids/k7.jpg"
+        image: " k7.jpg"
     },
     {
         id: 2,
         name: "Kurta",
         price: 499,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Kids/k8.jpg"
+        image: " k8.jpg"
     },
 	{
         id: 1,
         name: "Shirt & pant",
         price: 799,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Kids/k9.jpg"
+        image: " k9.jpg"
     },
     {
         id: 2,
         name: "Kid dress",
         price: 489,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Kids/k10.jpg"
+        image: " k10.jpg"
     },
     // Add more products here
 ];
@@ -71,61 +71,61 @@ const products1 = [
         id: 1,
         name: "Blazor",
         price: 1099,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Mens/m1.jpg"
+        image: " m1.jpg"
     },
     {
         id: 2,
         name: "Combo T-shirts",
         price: 740,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Mens/m2.jpg"
+        image: " m2.jpg"
     },
 	{
         id: 1,
         name: "Pink Shirt",
         price: 299,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Mens/m3.jpg"
+        image: " m3.jpg"
     },
     {
         id: 2,
         name: "Combo Shirts",
         price: 1199,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Mens/m4.jpg"
+        image: " m4.jpg"
     },
 	{
         id: 1,
         name: "kurtha",
         price: 455,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Mens/m5.jpg"
+        image: " m5.jpg"
     },
     {
         id: 2,
         name: "Black Tshirt",
         price: 199,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Mens/m6.jpg"
+        image: " m6.jpg"
     },
 	{
         id: 1,
         name: "Nike",
         price: 999,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Mens/m7.jpg"
+        image: " m7.jpg"
     },
     {
         id: 2,
         name: "Combo Pants",
         price: 658,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Mens/m8.jpg"
+        image: " m8.jpg"
     },
 	{
         id: 1,
         name: "Jeans pant",
         price: 499,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Mens/m9.jpg"
+        image: " m9.jpg"
     },
     {
         id: 2,
         name: "Formal Pants",
         price: 899,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Mens/m10.jpg"
+        image: " m10.jpg"
     },
 	,
     // Add more products here
@@ -138,61 +138,61 @@ const products2 = [
         id: 1,
         name: "Long gown",
         price: 599,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Women/w1.jpg"
+        image: " w1.jpg"
     },
     {
         id: 2,
         name: "Long Top",
         price: 799,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Women/w2.jpg"
+        image: " w2.jpg"
     },
 	{
         id: 1,
         name: "Chudidhar",
         price: 1200,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Women/w3.jpg"
+        image: " w3.jpg"
     },
     {
         id: 2,
         name: "Short jeans top",
         price: 349,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Women/w4.jpg"
+        image: " w4.jpg"
     },
 	{
         id: 1,
         name: "Full sleeves tops",
         price: 399,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Women/w5.jpg"
+        image: " w5.jpg"
     },
     {
         id: 2,
         name: "Jeans full length top",
         price: 299,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Women/w6.jpg"
+        image: " w6.jpg"
     },
 	{
         id: 1,
         name: "Hoodie with pants",
         price: 899,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Women/w7.jpg"
+        image: " w7.jpg"
     },
     {
         id: 2,
         name: "Top with pant",
         price: 999,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Women/w8.jpg"
+        image: " w8.jpg"
     },
 	{
         id: 1,
         name: "Top with pant",
         price: 799,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Women/w9.jpg"
+        image: " w9.jpg"
     },
     {
         id: 2,
         name: "Night Dress",
         price: 459,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Women/w10.jpg"
+        image: " w10.jpg"
     },
     // Add more products here
 ];
@@ -205,61 +205,61 @@ const products3 = [
         id: 1,
         name: "Bluetooth earbuds",
         price: 1099,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/electronics/e1.jpg"
+        image: " e1.jpg"
     },
     {
         id: 2,
         name: "Kitchen gadgets",
         price: 1999,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/electronics/e2.jpg"
+        image: " e2.jpg"
     },
 	{
         id: 1,
         name: "Speakers",
         price: 2499,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/electronics/e3.jpg"
+        image: " e3.jpg"
     },
     {
         id: 2,
         name: "Eletronics products",
         price: 15000,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/electronics/e4.jpg"
+        image: " e4.jpg"
     },
 	{
         id: 1,
         name: "Laptop",
         price: 55499,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/electronics/e5.jpg"
+        image: " e5.jpg"
     },
     {
         id: 2,
         name: "Hp Laptop",
         price: 69999,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/electronics/e6.jpg"
+        image: " e6.jpg"
     },
 	{
         id: 1,
         name: "LG Fridge",
         price: 15000,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/electronics/e7.jpg"
+        image: " e7.jpg"
     },
     {
         id: 2,
         name: "Samsung Fridge",
         price: 21999,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/electronics/e8.jpg"
+        image: " e8.jpg"
     },
 	{
         id: 1,
         name: "Washing Machine",
         price: 30699,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/electronics/e9.jpg"
+        image: " e9.jpg"
     },
     {
         id: 2,
         name: "LED Tv",
         price: 22000,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/electronics/e10.jpg"
+        image: " e10.jpg"
     },
     // Add more products here
 ];
@@ -272,61 +272,61 @@ const products4 = [
         id: 1,
         name: "Nike",
         price: 2999,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/shoes/s1.jpg"
+        image: " s1.jpg"
     },
     {
         id: 2,
         name: "Puma",
         price: 2999,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/shoes/s2.jpg"
+        image: " s2.jpg"
     },
 	{
         id: 1,
         name: "Campus",
         price: 699,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/shoes/s3.jpg"
+        image: " s3.jpg"
     },
     {
         id: 2,
         name: "Paragon",
         price: 399,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/shoes/s4.jpg"
+        image: " s4.jpg"
     },
 	{
         id: 1,
         name: "Casual",
         price: 989,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/shoes/s5.jpg"
+        image: " s5.jpg"
     },
     {
         id: 2,
         name: "Nike",
         price: 3399,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/shoes/s6.jpg"
+        image: " s6.jpg"
     },
 	{
         id: 1,
         name: "Woodland",
         price: 3999,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/shoes/s7.jpg"
+        image: " s7.jpg"
     },
     {
         id: 2,
         name: "Metro",
         price: 1599,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/shoes/s8.jpg"
+        image: " s8.jpg"
     },
 	{
         id: 1,
         name: "Liberty",
         price: 3569,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/shoes/s9.jpg"
+        image: " s9.jpg"
     },
     {
         id: 2,
         name: "Adidas",
         price: 6999,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/shoes/s10.jpg"
+        image: " s10.jpg"
     },
     // Add more products here
 ];
@@ -338,61 +338,61 @@ const products5 = [
         id: 1,
         name: "Panda",
         price: 399,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Toys/i1.jpg"
+        image: " i1.jpg"
     },
     {
         id: 2,
         name: "Dinosaur",
         price: 749,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Toys/i2.jpg"
+        image: " i2.jpg"
     },
 	{
         id: 1,
         name: "Pikachu",
         price: 699,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Toys/i3.jpg"
+        image: " i3.jpg"
     },
     {
         id: 2,
         name: "Robot DOg",
         price: 1999,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Toys/i4.jpg"
+        image: " i4.jpg"
     },
 	{
         id: 1,
         name: "Puppy",
         price: 499,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Toys/i5.jpg"
+        image: " i5.jpg"
     },
     {
         id: 2,
         name: "Doll",
         price: 199,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Toys/i6.jpg"
+        image: " i6.jpg"
     },
 	{
         id: 1,
         name: "barbie",
         price: 299,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Toys/i7.jpg"
+        image: " i7.jpg"
     },
     {
         id: 2,
         name: "Teddy",
         price: 499,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Toys/i8.jpg"
+        image: " i8.jpg"
     },
 	{
         id: 1,
         name: "Green Doll",
         price: 259,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Toys/i9.jpg"
+        image: " i9.jpg"
     },
     {
         id: 2,
         name: "Fidget Spinners",
         price: 699,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Toys/i10.jpg"
+        image: " i10.jpg"
     },
     // Add more products here
 ];
@@ -404,61 +404,61 @@ const products6 = [
         id: 1,
         name: "Pouch",
         price: 199,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Travel/t1.jpg"
+        image: " t1.jpg"
     },
     {
         id: 2,
         name: "Bages",
         price: 1999,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Travel/t2.jpg"
+        image: " t2.jpg"
     },
 	{
         id: 1,
         name: "Hand bag",
         price: 1399,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Travel/t3.jpg"
+        image: " t3.jpg"
     },
     {
         id: 2,
         name: "Mini Pouch",
         price: 999,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Travel/t4.jpg"
+        image: " t4.jpg"
     },
 	{
         id: 1,
         name: "Luggage bag",
         price: 2099,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Travel/t5.jpg"
+        image: " t5.jpg"
     },
     {
         id: 2,
         name: "Travel Bag",
         price: 3699,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Travel/t6.jpg"
+        image: " t6.jpg"
     },
 	{
         id: 1,
         name: "Mini Bag",
         price: 699,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Travel/t7.jpg"
+        image: " t7.jpg"
     },
     {
         id: 2,
         name: "Combo mini pouch's",
         price: 999,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Travel/t8.jpg"
+        image: " t8.jpg"
     },
 	{
         id: 1,
         name: "Bag",
         price: 199,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Travel/t9.jpg"
+        image: " t9.jpg"
     },
     {
         id: 2,
         name: "Designed Pouch",
         price: 499,
-        image: "file:///C:/Users/karth/OneDrive/Desktop/Task%202/Images/Travel/t10.jpg"
+        image: " t10.jpg"
     },
     // Add more products here
 ];
